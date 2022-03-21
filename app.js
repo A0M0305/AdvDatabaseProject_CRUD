ef62bf33774124b8357dd79a23e8a3e24e2a1c16
A0M0305 <79513200+A0M0305@users.noreply.github.com>
2022-03-21 21:03:14 +0000

    Added in to view logs on the terminal and dotenv.

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
-const express=require('express');
-const app=express();
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+
+const app = express();
+
+//use the confid file to change the port number, if the config number is in use it will use the default 8080
+dotenv.config({path:"config.env"})
+const PORT= process.env.PORT||8080
+
+//to log/see the request in the terminal
+app.use(morgan('tiny'));
+
+
 
 app.get("/", (req,res)=>{
     res.send("Crud Application");
 })
 
-app.listen(5033,()=>{console.log('Server is running on http://localhost:5033')});
+app.listen(PORT,()=>{console.log(`Server is running on http://localhost:${PORT}`)});
