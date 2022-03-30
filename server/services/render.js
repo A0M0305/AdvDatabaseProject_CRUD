76f672d860b2050cfb27505c9505c8066714578a
A0M0305 <79513200+A0M0305@users.noreply.github.com>
2022-03-30 21:40:19 +0100

    Updated user page

--- server/services/render.js
+++ server/services/render.js
@@ -8,7 +8,7 @@ exports.homeRoutes=(req,res) => {
     axios.get('http://localhost:5033/api/users')
         .then(function(response){
             console.log(response.data)
-            res.render("items",{users:response.data});
+            res.render("users",{users:response.data});
         })
         .catch(err =>{
             res.send(err);
